245a4f547698e27b676e1d890f633367ed10f3f7
Dmytro Danylov <52718097+Dmytro1996@users.noreply.github.com>
2021-03-31 20:05:55 +0300

    Create divideTwoIntegers.java

---
+++ sprint2/divideTwoIntegers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int divide(int dividend, int divisor) {        
+        String dividendAsStr=""+Math.abs((long)dividend);        
+        int beginIndex=0;
+        int endIndex=new String(""+Math.abs((long)divisor)).length();
+        long remainder=0;
+        String resultAsStr="";
+        while(endIndex<=dividendAsStr.length()){
+            long currentDividend=0;
+            if(beginIndex==0){ 
+                currentDividend=Long.valueOf(
+                  dividendAsStr.substring(beginIndex,endIndex));
+                beginIndex=endIndex;
+            } else{
+                currentDividend=Long.valueOf(
+                    remainder+dividendAsStr.substring(beginIndex,endIndex));
+                beginIndex++;
+            }
+            int resultOfDiv=0;
+            while(currentDividend>=Math.abs((long)divisor)){
+                currentDividend-=Math.abs((long)divisor);
+                resultOfDiv++;
+            }            
+            endIndex++;
+            resultAsStr+=resultOfDiv;
+            remainder=currentDividend;            
+        }
+        if(resultAsStr.isEmpty()){
+            return 0;
+        }
+        long result=Long.valueOf(resultAsStr);
+        if((dividend<0 && divisor>0) || (dividend>0 && divisor<0)){
+            result*=-1;
+        }
+        if(result>Integer.MAX_VALUE){
+           return Integer.MAX_VALUE;
+        }
+        return (int)result;
+    }
+}
